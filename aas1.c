4b69e2d7e893fa910651f5f9dfafe19d714c4ad5
Adam Ectrael <adam@Adams-MacBook-Air.local>
2025-09-02 16:24:14 +0700

    Upload 3 file tugas

---
+++ aas1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main () {
+    int length = 5;
+    int width = 3;
+    int area = 0;
+
+    area = length * width;
+
+    printf("Length = %d\n", length);
+    printf("Width = %d\n", width);
+    printf("Area of rectangle = %d\n", area);
+
+
+    return 0;
+}
